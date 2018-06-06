3ee20837f67f4e73183f543eec024fd0cd122b16
John Wen <John.Wen@ehealth.com>
2018-06-06 13:35:20 +0800

    fix

--- configure.py
+++ configure.py
@@ -1,4 +1,4 @@
 class Configure(object):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://johnw:qwe123@sjdevetsecapp01:9000/csrf"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://johnw:qwe123@xxxx:9000/csrf"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'you-never-guess'
